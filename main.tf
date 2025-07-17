e9102391e569c8547b2ca1d8e51734532ef589d4
Sot4 <Sot4.develop@gmail.com>
2025-07-17 17:01:59 +0900

    main.tfのAWSプロバイダーの設定を更新

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws" {
    region = "ap-northeast-1"
+   profile = "study-user"
+   assume_role {
+     role_arn = "arn:aws:iam::171824085810:role/TerraformExecutionRole"
+   }
 }
 
 resource "aws_instance" "example" {
