312d7159edc145031e8f545d1e243f5f6593e697
Sot4 <Sot4.develop@gmail.com>
2025-07-18 15:17:52 +0900

    簡易webサーバーとして起動するように変更

--- main.tf
+++ main.tf
@@ -9,4 +9,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-054400ced365b82a0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello,world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
